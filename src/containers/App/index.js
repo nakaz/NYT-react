b060778501aac8652df865d58a34ab4163eb84e6
Sean Nakamura <seanenakamura@gmail.com>
2017-04-04 22:08:15 -1000

    adds header component

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import Header from '../../components/Header'
 
 class App extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1>Hello World!</h1>
       </div>
     )
