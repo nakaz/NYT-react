3e3f8c4a8b660339c6155ae91fba78e5540b9858
Sean Nakamura <seanenakamura@gmail.com>
2017-04-04 22:52:19 -1000

    activates hmr in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-  entry:  path.resolve(__dirname, './src/index.js'),
+  entry: [
+    path.resolve(__dirname, './src/index.js')
+  ],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, './dist')
   },
   devServer: {
+    hot: true,
     inline: true,
     contentBase: './dist',
     stats: { colors: true },
@@ -25,10 +29,10 @@ module.exports = {
     ]
   },
   plugins: [
+    new webpack.HotModuleReplacementPlugin(),
     new HtmlWebpackPlugin({
       template: 'src/index.html',
       inject: true,
     })
   ]
-
 }
