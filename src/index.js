6cbf4b4df51c092625c2c3b4ec37c18a1f8447b3
Sean Nakamura <seanenakamura@gmail.com>
2017-04-04 22:04:54 -1000

    updates entry and creates app component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello world')
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './containers/App'
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
+
