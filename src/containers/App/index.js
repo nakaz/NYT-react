4fe20759e131e5e38a30e7d51393997ed4b08340
Sean Nakamura <seanenakamura@gmail.com>
2017-04-09 14:49:07 -1000

    updates entry with hmr

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -2,11 +2,26 @@ import React, { Component } from 'react';
 import Header from '../../components/Header'
 
 class App extends Component {
+  constructor(){
+    super()
+    this.state = {
+      counter: 0
+    }
+
+    this.increment = this.increment.bind(this)
+  }
+
+  increment() {
+    this.setState({counter: this.state.counter+10})
+  }
+
   render() {
     return (
       <div>
         <Header />
-        <h1>Hello World!</h1>
+        <h1>Hello sarah!!!!!!</h1>
+        <div>{this.state.counter}</div>
+        <button onClick={this.increment}>Increment</button>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { AppContainer } from 'react-hot-loader'
 import App from './containers/App'
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('app')
-)
+const render = (Component) =>{
+  ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+  )
+}
 
+render(App)
+
+if (module.hot) {
+  module.hot.accept('./containers/App', () => {
+    render(App)
+  })
+}
